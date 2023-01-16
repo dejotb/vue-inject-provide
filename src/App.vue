bfb3080a22e987085bf72e4c9025e1bd3d1c2315
dejotb <dejotb@gmail.com>
2023-01-16 19:39:28 +0100

    added provide / inject functionality

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base @select-topic="activateTopic"></knowledge-base>
   </div>
 </template>
 
@@ -32,6 +32,13 @@ export default {
       activeTopic: null,
     };
   },
+
+  provide() {
+    return {
+      topics: this.topics,
+    };
+  },
+
   methods: {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
